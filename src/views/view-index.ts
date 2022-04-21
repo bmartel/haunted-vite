aecf6046acc7b227b1fab994ea2a188ab8be0c3f
brandonmartel <brandonmartel@gmail.com>
2022-04-21 15:37:20 -0500

    use openprops for styling

--- src/views/view-index.ts
+++ src/views/view-index.ts
@@ -6,8 +6,6 @@ function ViewIndex() {
       :host {
         display: block;
         box-sizing: border-box;
-        padding: 0;
-        margin: 0;
         height: 100%;
         width: 100%;
       }
--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  ...(process.env.NODE_ENV === 'production'
-    ? null
-    : {
-        build: {
-          rollupOptions: {
-            output: {
-              format: 'cjs',
-              entryFileNames: `assets/[name].js`,
-              chunkFileNames: `assets/[name].js`,
-              assetFileNames: `assets/[name].[ext]`,
-            },
-          },
-        },
-      }),
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
@@ -51,6 +37,20 @@ export default defineConfig({
 
       workbox: {
         runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/unpkg\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'unpkg-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
             handler: 'CacheFirst',
